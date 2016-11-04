18361f99b386a1afdb43aa59f48ce6db610c76e3
Ben <waripod@hotmail.com>
2016-11-04 14:21:20 -0700

    Implemented spl_autoload_register in index.php to load PasteChart's classes so that they can be resolved when the website is loaded

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 namespace cs174\hw4;
 
+// Require Composer's autoload file (to autoload included vendors)
 require_once("vendor/autoload.php");
+
+// Implement autoloader for PasteChart's files
+spl_autoload_register(function ($className) {
+    // all files are in src folder, which makes it the prefix
+    $prefix = 'src/';
+
+    // then directory of class (since all classes in this website have namespaces
+    //   cs174\hw3\...  in folders named ...) we do some string manipulation
+    //   to extract the proper directory to jump to for requiring the class
+    //   (replacing backslash with forward slash, and only include name of class
+    //   starting after cs175\hw4\ in namespace to get file directory of the class)
+    $dir = str_replace('\\', '/', substr($className, strpos($className, "\\hw4\\") + 5)) . '.php';
+
+    // combine prefix and directory to pick out file to require
+    if(file_exists("$prefix$dir")) {
+        require_once("$prefix$dir");
+    }
+});
 
 /**
  * All links for the PasteChart site

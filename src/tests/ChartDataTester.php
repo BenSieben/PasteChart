14f548036f4ec336bbade0103418b95cb57a7228
Ben <waripod@hotmail.com>
2016-11-08 09:35:12 -0800

    Added a new test to ChartDataTester to check for each line having a label. Also switched all boolean results to add to a $results array in ChartDataTester which can be retrieved to see exactly which tests passed or failed in code. Also added getResultsKeys method which returns array of all keys used as indexes in the results array in ChartDataTester

--- src/tests/ChartDataTester.php
+++ src/tests/ChartDataTester.php
@@ -13,6 +13,7 @@ use cs174\hw4\configs\Config;
 class ChartDataTester extends \UnitTestCase {
 
     private $data; // the chart data to test
+    private $results; // an array which holds results of all the tests
 
     /**
      * Constructs a new DataTester
@@ -21,6 +22,7 @@ class ChartDataTester extends \UnitTestCase {
     public function __construct($data) {
         parent::__construct();
         $this->data = $data;
+        $this->results = array();
     }
 
     /**
@@ -28,7 +30,8 @@ class ChartDataTester extends \UnitTestCase {
      * exceed the maximum number of allowed lines
      */
     public function testNumDataLines() {
-        $this->assertTrue(substr_count($this->data, "\n") < Config::MAX_DATA_LINES, "There are too many data lines!");
+        $this->results['has_acceptable_num_lines'] = substr_count($this->data, "\n") < Config::MAX_DATA_LINES;
+        $this->assertTrue($this->results['has_acceptable_num_lines'], "There are too many data lines!");
     }
 
     /**
@@ -39,13 +42,13 @@ class ChartDataTester extends \UnitTestCase {
     public function testNumValuesPerDataLine() {
         $split_lines = explode("\n", $this->data);
         $first_line_comma_count = substr_count($split_lines[0], ","); // count number of commas in first line
-        $all_lines_match = true;
+        $this->results['all_lines_have_same_num_values'] = true;
         $max_comma_count = $first_line_comma_count;
         foreach($split_lines as $line) { // make sure each line has same number of commas
             $line_comma_count = substr_count($line, ",");
             if($line_comma_count !== $first_line_comma_count) {
                 // if a line does not match same number of values as first line, we know all lines are not matching
-                $all_lines_match = false;
+                $this->results['all_lines_have_same_num_values'] = false;
             }
             if($max_comma_count < $line_comma_count) {
                 // update max comma count if we found a line with more commas than currently in max comma count
@@ -54,9 +57,12 @@ class ChartDataTester extends \UnitTestCase {
         }
         // assert that all lines have matching length and that the maximum comma count (i.e., the maximum
         //   value count between all the data lines) is within maximum specification
-        $this->assertTrue($all_lines_match, "Not all lines match in number of values in data!");
-        $this->assertTrue($max_comma_count < Config::MAX_VALUES_PER_LINE, "Not all lines are within maximum number of allowed values!");
-        $this->assertTrue($max_comma_count > 0, "There must be at least one data value per line!");
+        $this->assertTrue($this->results['all_lines_have_same_num_values'], "Not all lines match in number of values in data!");
+
+        $this->results['no_line_exceeds_max_count'] = $max_comma_count < Config::MAX_VALUES_PER_LINE;
+        $this->results['each_line_has_at_least_one_value'] = $max_comma_count > 0;
+        $this->assertTrue($this->results['no_line_exceeds_max_count'], "Not all lines are within maximum number of allowed values!");
+        $this->assertTrue($this->results['each_line_has_at_least_one_value'], "There must be at least one data value per line!");
     }
 
     /**
@@ -65,15 +71,15 @@ class ChartDataTester extends \UnitTestCase {
      */
     public function testDataLineLength() {
         $split_lines = explode("\n", $this->data);
-        $all_lines_acceptable_length = true;
+        $this->results['all_lines_acceptable_length'] = true;
         foreach($split_lines as $line) { // make sure each line has same number of commas
             if(strlen($line) > Config::MAX_DATA_LINE_LENGTH) {
                 // if a line exceeds maximum data line length in Config, this test fails
-                $all_lines_acceptable_length = false;
+                $this->results['all_lines_acceptable_length'] = false;
                 break;
             }
         }
-        $this->assertTrue($all_lines_acceptable_length, "Not all lines stay under the maximum number of allowed characters!");
+        $this->assertTrue($this->results['all_lines_acceptable_length'], "Not all lines stay under the maximum number of allowed characters!");
     }
 
     /**
@@ -83,7 +89,7 @@ class ChartDataTester extends \UnitTestCase {
     public function testDataLineValues() {
         $double_regex = '/[-+]?[0-9]*\.?[0-9]+/'; // matches any number value the user can input
         $split_lines = explode("\n", $this->data);
-        $all_values_double = true;
+        $this->results['all_values_double'] = true;
         for($i = 0; $i < count($split_lines); $i++) {
             $line = $split_lines[$i];
             // for each line, extract all the values and test all the non x-value
@@ -94,7 +100,7 @@ class ChartDataTester extends \UnitTestCase {
                     if(preg_match($double_regex, $line_contents[$j]) !== 1) {
                         // we found something that should be a value to graph that is not
                         //   a number if we get here, so report this
-                        $all_values_double = false;
+                        $this->results['all_values_double'] = false;
                         break;
                     }
                 }
@@ -102,14 +108,55 @@ class ChartDataTester extends \UnitTestCase {
                     if(strlen($line_contents[$j]) !== 0 && preg_match($double_regex, $line_contents[$j]) !== 1) {
                         // we found something that should be a value to graph that is not
                         //   a number if we get here, so report this
-                        $all_values_double = false;
+                        $this->results['all_values_double'] = false;
                         break;
                     }
                 }
 
             }
         }
-        $this->assertTrue($all_values_double, "Not all given data values are valid numbers!");
+        $this->assertTrue($this->results['all_values_double'], "Not all given data values are valid numbers!");
+    }
+
+    /**
+     * This unit test checks that each line of code has something
+     * for a a x-value (i.e., the label for the values on the line)
+     */
+    public function testDataXValues() {
+        $split_lines = explode("\n", $this->data);
+        $this->results['all_lines_have_labels'] = true;
+        foreach($split_lines as $line) {
+            $label = explode(",", $line)[0];
+            if(strlen($label) === 0) { // if label is 0 characters long, that means there is no label on this line
+                $this->results['all_lines_have_labels'] = false;
+            }
+        }
+        $this->assertTrue($this->results['all_lines_have_labels'], "Not all lines have labels for the value(s)!");
+    }
+
+    /**
+     * Returns the results that were obtained from all the tests
+     * Use this after calling the run function to see specifically
+     * which tests failed in code
+     * @return Array<boolean> results from all the tests
+     */
+    public function getResults() {
+        return $this->results;
+    }
+
+    /**
+     * Returns a string array of all keys used in ChartDataTester's
+     * $result array
+     * @return Array<String> all keys in the $result array
+     */
+    public function getResultsKeys() {
+        return ['has_acceptable_num_lines',
+            'all_lines_have_same_num_values',
+            'no_line_exceeds_max_count',
+            'each_line_has_at_least_one_value',
+            'all_lines_acceptable_length',
+            'all_values_double',
+            'all_lines_have_labels'];
     }
 
 }

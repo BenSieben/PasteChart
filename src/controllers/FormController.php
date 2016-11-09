6c7ac4204fe1aa02d9556fa821940dbe29a957e2
Ben <waripod@hotmail.com>
2016-11-08 17:23:01 -0800

    Started implementation of server-side form checking in FormController

--- src/controllers/FormController.php
+++ src/controllers/FormController.php
@@ -2,6 +2,7 @@
 namespace cs174\hw4\controllers;
 use \cs174\hw4\configs\Config as Config;
 use \cs174\hw4\models\ChartModel as ChartModel;
+use \cs174\hw4\tests\ChartDataTester as ChartDataTester;
 
 /**
  * Class FormController
@@ -24,8 +25,11 @@ class FormController extends Controller {
             $title = $_REQUEST['title'];
             $data = str_replace("\r", "", $_REQUEST['chartData']); // remove return carriages from chart data if they exist
             $data = preg_replace("/\n+/", "\n", $data); // remove extra newlines between lines
-            $this->verifyChartForm($title, $data); // TODO do actual checking of data instead of jumping straight to chart view
             $md5 = hash("md5", $data);
+
+            // TODO do actual checking of data instead of jumping straight to chart view
+            $this->verifyChartForm($title, $data);
+
             // insert data into database
             $cm = new ChartModel();
             $result = $cm->insertChartEntry($md5, $title, $data);
@@ -67,7 +71,73 @@ class FormController extends Controller {
         // 1 to check number of items in tuple
         // 2 to check number of lines and line length constraints
 
-        //Step 1: use the ChartDataTester to run some SimpleTest unit tests on the data
+        // this will keep track of all errors found during the check, and will be used
+        //   later to determine if the form should be rejected or not
+        $errors = [];
+
+        $newTitle = $title; // $newTitle = title to give user if their form gets rejected
+
+        // Step 1: check the title for validity
+        if(strlen($title) === 0) {
+            array_push($errors, "Error: title needs to have at least one character<br />");
+        }
+        else if(strlen($title) > Config::MAX_TITLE_LENGTH) {
+            array_push($errors, "Error: title exceeds maximum character length of " . Config::MAX_TITLE_LENGTH .
+                "Excess characters have been trimmed off.<br />");
+            $newTitle = substr($title, 0, Config::MAX_TITLE_LENGTH);
+        }
+
+        // Step 2: use the ChartDataTester to run some SimpleTest unit tests on the data
+        $cdt = new ChartDataTester($data);
+        $cdt->run(new \SimpleReporter());
+        $cdt_results = $cdt->getResults();
+
+        // check cdt_results to see if any problematic lines were found
+        // if so, first find out which problem(s) occurred
+        // also record which lines caused problems
+        $problem_keys = []; // will hold the key(s) from cdt_results that indicate a problem with data line(s)
+        $problem_lines = [];
+        foreach($cdt_results as $key => $value) {
+
+        }
+
+        // make new data which does not have all the problematic lines to
+        //   create $newData, which will be used when redirecting the user
+        //   back to the landing page
+
+        // Step 3: determine if any errors were detected in testing, and if so, redirect
+        //   user back to landing page with cleaned form values (and error messages)
+    }
+
+    /**
+     * This private helper function generates an error message for the
+     * given key (a key which is in ChartDataTester's key list)
+     * @param $key
+     * @return String the error message associated with the given key,
+     * or the empty string if an unknown key is given
+     */
+    private function getErrorString($key) {
+        switch($key) {
+            case 'lines_after_maximum_allowed_lines':
+                return "Error: maximum number of lines exceeded. Excess lines have been removed<br />";
+            case 'lines_with_non_matching_values':
+                return "Error: lines with number of values not matching the first line have been " .
+                    "detected. These lines have been removed. <br />";
+            case 'lines_with_too_many_values':
+                return "Error: lines with too many values (more than " . Config::MAX_VALUES_PER_LINE .
+                    ") detected. These lines have been removed<br />";
+            case 'lines_with_too_few_values':
+                return "Error: lines with no values detected. These lines have been removed <br />";
+            case 'lines_with_too_many_characters':
+                return "Error: lines exceeding " . Config::MAX_DATA_LINE_LENGTH . " characters have been " .
+                    "detected. These lines have been removed<br />";
+            case 'lines_with_non_numeric_values':
+                return "Error: lines with non-numeric values have been detected. These lines have been removed<br />";
+            case 'lines_with_invalid_labeling':
+                return "Error: lines missing a label have been detected. These lines have been removed<br />";
+            default:
+                return "";
+        }
     }
 }
 ?>
